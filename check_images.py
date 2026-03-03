799d4b8d3c49ec15a3f43917af1e8ff697faae16
lakshitaa <lakshitaa2112@gmail.com>
2026-03-04 03:53:51 +0530

    Publish site 04/03/2026 03:53

--- check_images.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import glob, os
-for path in glob.glob('pictures/*.webp'):
-    img = Image.open(path)
-    print(path, img.format, img.size)
-    newpath = os.path.splitext(path)[0] + '.jpg'
-    img.convert('RGB').save(newpath, 'JPEG', quality=85)
-    print('converted', path, '->', newpath)
